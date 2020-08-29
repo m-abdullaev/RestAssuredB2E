8585b4df6ef96829d3648cc45cacc3ee1ee5111e
daulet2030 <daulet2030@gmail.com>
2020-08-29 07:30:24 +0600

    task2 solution

--- src/test/java/TaskSolutions.java
+++ src/test/java/TaskSolutions.java
@@ -1,7 +1,9 @@
 import io.restassured.http.ContentType;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 public class TaskSolutions {
     @Test
@@ -13,7 +15,26 @@ public class TaskSolutions {
                 .statusCode(203)
                 .contentType(ContentType.TEXT)
         ;
+    }
 
+    @Test
+    public void task2() {
+//        String body = given()
+//                .when()
+//                .get("https://httpstat.us/418")
+//                .then()
+//                .statusCode(418)
+//                .contentType(ContentType.TEXT)
+//                .extract().asString();
+//
+//        Assert.assertEquals(body, "418 I'm a teapot");
 
+        given()
+                .when()
+                .get("https://httpstat.us/418")
+                .then()
+                .statusCode(418)
+                .contentType(ContentType.TEXT)
+                .body(equalTo("418 I'm a teapot"));
     }
 }
