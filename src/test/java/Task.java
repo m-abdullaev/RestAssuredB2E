e491d70f643976a84b1a231b4295201bf0dc5848
daulet2030 <daulet2030@gmail.com>
2020-08-29 07:14:42 +0600

    task2

--- src/test/java/Task.java
+++ src/test/java/Task.java
@@ -4,4 +4,11 @@ public class Task {
      * expect status 203
      * expect content type text
      * **/
+
+    /** Task 2
+     * create a request to https://httpstat.us/418
+     * expect status 418
+     * expect content type text
+     * expect body to be equal to "418 I'm a teapot"
+     * **/
 }
