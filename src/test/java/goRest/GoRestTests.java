63686837c029a3a7f90fa602850e4a8e395b3097
daulet2030 <daulet2030@gmail.com>
2020-09-01 06:50:41 +0600

    extacting multiple times

--- src/test/java/goRest/GoRestTests.java
+++ src/test/java/goRest/GoRestTests.java
@@ -2,6 +2,8 @@ package goRest;
 
 import goRest.model.User;
 import io.restassured.http.ContentType;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -25,6 +27,29 @@ public class GoRestTests {
                 //extracting users list
                 .extract().jsonPath().getList("data", User.class);
 
+        for (User user : userList) {
+            System.out.println(user);
+        }
+    }
+
+    @Test
+    public void getUsersExtactingMultipleTimes() {
+        ExtractableResponse<Response> extract = given()
+                .when()
+                .get("https://gorest.co.in/public-api/users")
+                .then()
+                .log().body()
+                //assertions
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("code", equalTo(200))
+                .body("data", not(empty()))
+                //extracting users list
+                .extract();
+
+        List<User> userList = extract.jsonPath().getList("data", User.class);
+        int code = extract.jsonPath().getInt("code");
+        System.out.println("Code: " + code);
         for (User user : userList) {
             System.out.println(user);
         }
