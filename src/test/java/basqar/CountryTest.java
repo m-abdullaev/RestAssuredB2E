c4e6f74375754600c33588e33a705c8cd2569d6c
daulet2030 <daulet2030@gmail.com>
2020-09-02 06:50:54 +0600

    basqar create country

--- src/test/java/basqar/CountryTest.java
+++ src/test/java/basqar/CountryTest.java
@@ -1,22 +1,29 @@
 package basqar;
 
+import basqar.model.Country;
 import io.restassured.http.ContentType;
 import io.restassured.http.Cookies;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
 
 public class CountryTest {
 
     private Cookies cookies;
+    private String randomGenName;
+    private String randomGenCode;
 
     @BeforeClass
     public void init() {
         baseURI = "https://test.basqar.techno.study";
+
+        randomGenName = RandomStringUtils.randomAlphabetic(8);
+        randomGenCode = RandomStringUtils.randomAlphabetic(4);
 
         Map<String, String> credentials = new HashMap<>();
         credentials.put("username","daulet2030@gmail.com");
@@ -30,5 +37,22 @@ public class CountryTest {
                 .then()
                 .statusCode(200)
                 .extract().response().getDetailedCookies();
+    }
+
+    @Test
+    public void createCountry(){
+        Country country = new Country();
+        country.setName(randomGenName);
+        country.setCode(randomGenCode);
+
+        given()
+                .cookies(cookies)
+                .contentType(ContentType.JSON)
+                .body(country)
+                .when()
+                .post("/school-service/api/countries")
+                .then()
+                .statusCode(201)
+                ;
     }
 }
