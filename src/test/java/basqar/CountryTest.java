63c5e21afff4ff4a13d4338316b5d2231c4163a1
daulet2030 <daulet2030@gmail.com>
2020-09-02 06:42:23 +0600

    basqar init authentication

--- src/test/java/basqar/CountryTest.java
+++ src/test/java/basqar/CountryTest.java
@@ -1,8 +1,34 @@
 package basqar;
 
+import io.restassured.http.ContentType;
+import io.restassured.http.Cookies;
+import org.testng.annotations.BeforeClass;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class CountryTest {
 
+    private Cookies cookies;
+
+    @BeforeClass
+    public void init() {
+        baseURI = "https://test.basqar.techno.study";
+
+        Map<String, String> credentials = new HashMap<>();
+        credentials.put("username","daulet2030@gmail.com");
+        credentials.put("password","TechnoStudy123@");
+
+        cookies = given()
+                .body(credentials)
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/auth/login")
+                .then()
+                .statusCode(200)
+                .extract().response().getDetailedCookies();
+    }
 }
