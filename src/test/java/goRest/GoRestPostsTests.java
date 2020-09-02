11d9dc96892cc695faf71e30f4035bd6867d19a7
daulet2030 <daulet2030@gmail.com>
2020-09-02 06:04:54 +0600

    refactoring baseURI

--- src/test/java/goRest/GoRestPostsTests.java
+++ src/test/java/goRest/GoRestPostsTests.java
@@ -1,6 +1,8 @@
 package goRest;
+
 import goRest.model.Posts;
 import io.restassured.http.ContentType;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -8,15 +10,21 @@ import java.util.Random;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
+
 public class GoRestPostsTests {
 
     private int postId;
+
+    @BeforeClass
+    public void init() {
+        baseURI = "https://gorest.co.in/public-api/posts";
+    }
 
     @Test()
     public void getPosts() {
         List<Posts> postsList = given()
                 .when()
-                .get("https://gorest.co.in/public-api/posts")
+                .get()
                 .then()
                 .statusCode(200)
                 .contentType(ContentType.JSON)
@@ -38,7 +46,7 @@ public class GoRestPostsTests {
                 .body("{\"user_id\":\"" + getRandomUserId() + "\", \"title\": \"Techno\", \"body\":\"Male\"}")
                 .when()
                 //action
-                .post("https://gorest.co.in/public-api/posts")
+                .post()
                 .then()
                 .log().body()
                 //validations
@@ -50,11 +58,11 @@ public class GoRestPostsTests {
     }
 
     @Test(dependsOnMethods = "createPost")
-    public void getPostById(){
+    public void getPostById() {
         given()
                 .pathParam("postId", postId)
                 .when()
-                .get("https://gorest.co.in/public-api/posts/{postId}")
+                .get("/{postId}")
                 .then()
                 .statusCode(200)
                 .contentType(ContentType.JSON)
@@ -64,15 +72,15 @@ public class GoRestPostsTests {
     }
 
     @Test(dependsOnMethods = "createPost")
-    public void updatePostById(){
+    public void updatePostById() {
         String updateText = "Update Post Test";
         given()
                 .header("Authorization", "Bearer 55b19d86844d95532f80c9a2103e1a3af0aea11b96817e6a1861b0d6532eef47")
                 .contentType(ContentType.JSON)
-                .body("{\"body\": \""+updateText+"\"}")
-                .pathParam("postId",postId)
+                .body("{\"body\": \"" + updateText + "\"}")
+                .pathParam("postId", postId)
                 .when()
-                .put("https://gorest.co.in/public-api/posts/{postId}")
+                .put("/{postId}")
                 .then()
                 .statusCode(200)
                 .body("code", equalTo(200))
@@ -80,12 +88,12 @@ public class GoRestPostsTests {
     }
 
     @Test(dependsOnMethods = "createPost", priority = 1)
-    public void deletePostById(){
+    public void deletePostById() {
         given()
                 .header("Authorization", "Bearer 55b19d86844d95532f80c9a2103e1a3af0aea11b96817e6a1861b0d6532eef47")
-                .pathParam("postId",postId)
+                .pathParam("postId", postId)
                 .when()
-                .delete("https://gorest.co.in/public-api/posts/{postId}")
+                .delete("/{postId}")
                 .then()
                 .statusCode(200)
                 .body("code", equalTo(204))
@@ -97,7 +105,7 @@ public class GoRestPostsTests {
         given()
                 .pathParam("postId", postId)
                 .when()
-                .get("https://gorest.co.in/public-api/posts/{postId}")
+                .get("/{postId}")
                 .then()
                 .statusCode(200)
                 .contentType(ContentType.JSON)
